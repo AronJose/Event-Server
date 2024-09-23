04d5d073a7c63cb8a2ec96661cc7b1b2ddf31f3f
aron.jose.tblr <aron.jose.tblr@gmail.co>
2024-09-23 21:01:09 +0530

    video upload multer file

--- Helper/multerImageUpload.js
+++ Helper/multerImageUpload.js
@@ -2,8 +2,6 @@ const multer = require('multer');
 const path = require('path');
 
 
-console.log("inside routerrrrrrrrrrrrrrrrr")
-
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/'); // Save files in the 'uploads' folder
@@ -14,11 +12,23 @@ const storage = multer.diskStorage({
     }
 });
 
-console.log(storage, "storage")
+const fileFilter = (req, file, cb) => {
+    const allowedFileTypes = /jpeg|jpg|png|gif|mp4|mov|avi/; // Allow images and video formats
+    const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedFileTypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        return cb(null, true);
+    } else {
+        cb(new Error('Only images and videos are allowed!')); // Reject file
+    }
+};
+
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 100000000 }
+    limits: { fileSize: 500 * 1024 * 1024 },
+    fileFilter: fileFilter
 });
 
 // Middleware for single file upload
@@ -27,7 +37,15 @@ const singleUpload = upload.single('image');
 // Middleware for multiple files upload
 const multipleUpload = upload.array('images', 8); // Limit to 10 files 
 
+// Middleware for single video upload
+const singleVideoUpload = upload.single('video');
+
+// Middleware for multiple video uploads (limit to 5 files)
+const multipleVideoUpload = upload.array('videos', 5);
+
 module.exports = {
     singleUpload,
-    multipleUpload
+    multipleUpload, 
+    singleVideoUpload,
+    multipleVideoUpload
 };
